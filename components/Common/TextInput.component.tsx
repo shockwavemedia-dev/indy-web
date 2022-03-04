79fda5ea61ab760f113e1efbe9d2908ca22f883a
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-04 08:46:03 +0800

    Add button aria-label

--- components/Common/TextInput.component.tsx
+++ components/Common/TextInput.component.tsx
@@ -48,6 +48,7 @@ const TextInput = ({
         {type === 'password' && (
           <button
             className="mr-[10px] flex cursor-pointer justify-center bg-transparent p-0"
+            aria-label="Show Password"
             onClick={toggleShowPassword}
           >
             <EyeOpenIcon />
