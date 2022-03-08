f828ef8d027169ea75e9f03ae4c241bd6d209254
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-08 12:07:09 +0800

    Rename 'isSubmitting' prop to 'disabled'

--- components/Common/Button.component.tsx
+++ components/Common/Button.component.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from 'react'
 const Button = ({
   type = 'button',
   ariaLabel,
-  isSubmitting,
+  disabled = false,
   children,
   isLight = false,
 }: {
-  type: 'button' | 'submit'
+  type?: 'button' | 'submit'
   ariaLabel: string
-  isSubmitting: boolean
+  disabled?: boolean
   children: ReactNode
   isLight?: boolean
 }) => {
@@ -17,10 +17,10 @@ const Button = ({
     <button
       type={type}
       aria-label={ariaLabel}
-      disabled={isSubmitting}
-      className={`flex h-[44px] flex-1 cursor-pointer select-none items-center justify-center space-x-[8px] rounded-[4px] font-inter text-[14px] font-medium ${
-        isLight ? 'text-raisinblack' : 'bg-raisinblack text-white'
-      }`}
+      disabled={disabled}
+      className={`flex h-[44px] flex-1 cursor-pointer select-none items-center justify-center space-x-[8px] rounded-[4px] font-inter text-[14px] font-medium 
+      ${isLight ? 'text-raisinblack' : 'bg-raisinblack text-white'}
+      `}
     >
       {children}
     </button>
