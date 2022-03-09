ce9a3efcd501a0dd0d855b4dc8cf8a451ffe35e4
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-09 08:20:50 +0800

    Add className prop to svg

---
+++ components/Common/Icons/CaretDown.icon.tsx
@@ -0,0 +1,22 @@
+const CaretDownIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M13 6L8 11L3 6"
+        stroke="#717583"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default CaretDownIcon
--- components/Common/Icons/CaretRight.icon.tsx
+++ components/Common/Icons/CaretRight.icon.tsx
@@ -1,9 +1,10 @@
-const CaretRightIcon = () => {
+const CaretRightIcon = ({ className }: { className: string }) => {
   return (
     <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
+        className={className}
         d="M6.5 3L11.5 8L6.5 13"
-        stroke="white"
+        stroke="black"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
