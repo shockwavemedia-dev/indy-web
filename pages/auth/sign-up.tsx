798396020e7dfd39cafb6394abdc7d42cd5fcfb2
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-18 05:18:05 +0800

    Change header casing

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -70,7 +70,7 @@ const SignUp: NextPageWithLayout = () => {
       },
       {
         headers: {
-          authorization: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       }
     )
