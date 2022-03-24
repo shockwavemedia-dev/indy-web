a8b75f2ef565e48adf939c28efcd742abe471291
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-24 10:10:26 +0800

    Update text input component

--- components/Common/TextInput.component.tsx
+++ components/Common/TextInput.component.tsx
@@ -5,52 +5,47 @@ import EyeOpenIcon from './Icons/EyeOpen.icon'
 const TextInput = ({
   type = 'text',
   name,
-  label,
   Icon,
   placeholder,
   disableAutoComplete = false,
+  className,
 }: {
   type?: 'text' | 'email' | 'password'
   name: string
-  label: string
   Icon: ComponentType<{ className: string }>
   placeholder: string
   disableAutoComplete?: boolean
+  className?: string
 }) => {
   const [isShowPassword, setShowPassword] = useState(false)
 
   const toggleShowPassword = () => setShowPassword(!isShowPassword)
 
   return (
-    <div className="flex w-full flex-col">
-      <label className="mb-2 font-inter text-xs font-normal text-mineshaft" htmlFor={name}>
-        {label}
+    <div className={`relative flex w-full items-center ${className}`}>
+      <label className="absolute ml-6" htmlFor={name}>
+        <Icon className="stroke-lavender-gray" />
       </label>
-      <div className="h-11.5 flex items-center overflow-hidden rounded border border-solid border-ebonyclay border-opacity-10">
-        <label className="flex h-full items-center pl-2.5 pr-2" htmlFor={name}>
-          <Icon className="stroke-black" />
-        </label>
-        <Field
-          className="h-full w-full pr-2.5 font-inter text-sm font-normal placeholder-stormgray"
-          type={isShowPassword ? 'text' : type}
-          name={name}
-          id={name}
-          spellCheck={false}
-          placeholder={placeholder}
-          autoComplete={disableAutoComplete ? (type === 'text' ? 'off' : 'new-password') : 'on'}
-        />
-        {type === 'password' && (
-          <button
-            className="mr-2.5 flex cursor-pointer justify-center bg-transparent p-0"
-            aria-label="Show Password"
-            onClick={toggleShowPassword}
-            tabIndex={-1}
-            type="button"
-          >
-            <EyeOpenIcon className="stroke-black" />
-          </button>
-        )}
-      </div>
+      <Field
+        className="min-h-12.5 w-full rounded-xl px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray ring-offset-0 focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+        type={isShowPassword ? 'text' : type}
+        name={name}
+        id={name}
+        spellCheck={false}
+        placeholder={placeholder}
+        autoComplete={disableAutoComplete ? (type === 'text' ? 'off' : 'new-password') : 'on'}
+      />
+      {type === 'password' && (
+        <button
+          className="absolute right-0 mr-6"
+          aria-label="Show Password"
+          onClick={toggleShowPassword}
+          tabIndex={-1}
+          type="button"
+        >
+          <EyeOpenIcon className="stroke-lavender-gray" />
+        </button>
+      )}
     </div>
   )
 }
