a7f22ec419827bce1dfef7c6f9411bf12b6e576b
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-25 07:03:55 +0800

    Update text area component

--- components/Common/TextAreaInput.component.tsx
+++ components/Common/TextAreaInput.component.tsx
@@ -12,13 +12,12 @@ const TextAreaInput = ({
   placeholder: string
   className?: string
 }) => (
-  <div className={`relative flex w-full items-center ${className}`}>
-    <Icon className="absolute ml-6 stroke-lavender-gray" />
+  <div className={`flex ${className}`}>
+    <Icon className="absolute ml-6 mt-4 stroke-lavender-gray" />
     <Field
-      className="z-10 h-full w-full resize-none rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+      className="no-sc z-10 min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
       component="textarea"
       name={name}
-      id={name}
       spellCheck={false}
       placeholder={placeholder}
     />
