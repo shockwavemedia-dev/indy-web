5065679d52e83621ee6ce625cca33f19565c1d90
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-24 10:10:34 +0800

    Update button component

--- components/Common/Button.component.tsx
+++ components/Common/Button.component.tsx
@@ -7,6 +7,7 @@ const Button = ({
   children,
   isLight = false,
   onClick,
+  className,
 }: {
   type?: 'button' | 'submit'
   ariaLabel: string
@@ -14,14 +15,13 @@ const Button = ({
   children: ReactNode
   isLight?: boolean
   onClick?: MouseEventHandler<HTMLButtonElement>
+  className?: string
 }) => (
   <button
     type={type}
     aria-label={ariaLabel}
     disabled={disabled}
-    className={`flex h-11 flex-1 cursor-pointer items-center justify-center space-x-2 rounded font-inter text-sm font-medium ${
-      isLight ? 'border border-solid border-tuna text-shark' : 'bg-shark text-white'
-    }`}
+    className={`flex min-h-12.5 w-full items-center justify-center space-x-2 rounded-xl bg-jungle-green font-urbanist text-base font-semibold text-white ${className}`}
     onClick={onClick}
   >
     {children}
