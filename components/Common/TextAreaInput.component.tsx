720990fb2c65f9e925d71f7d114b65d4eab981d2
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-25 06:14:51 +0800

    Textarea component update partial commit

--- components/Common/TextAreaInput.component.tsx
+++ components/Common/TextAreaInput.component.tsx
@@ -3,32 +3,25 @@ import { ComponentType } from 'react'
 
 const TextAreaInput = ({
   name,
-  label,
   Icon,
   placeholder,
+  className,
 }: {
   name: string
-  label: string
   Icon: ComponentType<{ className: string }>
   placeholder: string
+  className?: string
 }) => (
-  <div className="flex w-full flex-col">
-    <label className="mb-2 font-inter text-xs font-normal text-mineshaft" htmlFor={name}>
-      {label}
-    </label>
-    <div className="flex h-35 items-start overflow-hidden rounded border border-solid border-ebonyclay border-opacity-10">
-      <label htmlFor={name} className="h-full pt-3.5 pl-2.5 pr-2">
-        <Icon className="stroke-black" />
-      </label>
-      <Field
-        className="h-full w-full resize-none border-none py-3.5 pr-2.5 font-inter text-sm font-normal placeholder-stormgray outline-none"
-        component="textarea"
-        name={name}
-        id={name}
-        spellCheck={false}
-        placeholder={placeholder}
-      />
-    </div>
+  <div className={`relative flex w-full items-center ${className}`}>
+    <Icon className="absolute ml-6 stroke-lavender-gray" />
+    <Field
+      className="z-10 h-full w-full resize-none rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+      component="textarea"
+      name={name}
+      id={name}
+      spellCheck={false}
+      placeholder={placeholder}
+    />
   </div>
 )
 
