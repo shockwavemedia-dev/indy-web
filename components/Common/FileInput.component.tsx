96c1eb5f05308b3279f8ba5cd4824f3ed78c61a9
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-25 07:04:05 +0800

    Update file input component

--- components/Common/FileInput.component.tsx
+++ components/Common/FileInput.component.tsx
@@ -1,51 +1,48 @@
 import Dropzone from 'react-dropzone'
 
 const FileInput = ({
-  label,
   name,
   setFieldValue,
   multiple = false,
+  className,
 }: {
-  label: string
   name: string
   setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void
   multiple?: boolean
+  className?: string
 }) => {
   const dropFiles = (files: Array<File>) => setFieldValue(name, multiple ? files : files.pop())
 
   return (
-    <div className="flex w-full flex-col">
-      <label className="mb-2 font-inter text-xs font-normal text-mineshaft">{label}</label>
-      <Dropzone onDrop={dropFiles} noClick noKeyboard multiple={multiple}>
-        {({ getRootProps, getInputProps, open, isDragActive }) => (
-          <div
-            {...getRootProps()}
-            className={`flex h-35 cursor-auto items-center justify-center overflow-hidden rounded border border-dashed border-manatee ${
-              isDragActive ? 'bg-gallery' : 'bg-wildsand'
-            }`}
-          >
-            <div className="cursor absolute flex flex-col items-center">
-              <div className="mb-2 font-inter text-sm font-semibold text-mineshaft">
-                Drag &amp; drop you assets, or{' '}
-                <button
-                  className=" font-medium text-stormgray underline underline-offset-1"
-                  name="Browse Assets"
-                  onClick={open}
-                  type="button"
-                >
-                  browse it
-                </button>
-              </div>
-              <div className="text-center font-inter text-xs font-normal text-frenchgray">
-                Files Supported: MP3, MP4, AVI, MOV, FLV. <br />
-                Maximum size: 250 MB
-              </div>
+    <Dropzone onDrop={dropFiles} noClick noKeyboard multiple={multiple}>
+      {({ getRootProps, getInputProps, open, isDragActive }) => (
+        <div
+          {...getRootProps()}
+          className={`flex min-h-35 cursor-auto items-center justify-center overflow-hidden rounded-xl border border-dashed border-lavender-gray ${
+            isDragActive ? 'bg-honeydew' : 'bg-ghost-white'
+          } ${className}`}
+        >
+          <div className="cursor absolute flex flex-col items-center">
+            <div className="mb-1.5 font-urbanist text-sm font-medium text-onyx">
+              Drag &amp; drop you assets, or&nbsp;
+              <button
+                className="font-semibold text-jungle-green underline underline-offset-1"
+                name="Browse Assets"
+                onClick={open}
+                type="button"
+              >
+                browse it
+              </button>
             </div>
-            <input {...getInputProps()} />
+            <div className="text-center font-urbanist text-xs font-medium text-metallic-silver">
+              Files Supported: MP3, MP4, AVI, MOV, FLV. <br />
+              Maximum size: 250 MB
+            </div>
           </div>
-        )}
-      </Dropzone>
-    </div>
+          <input {...getInputProps()} />
+        </div>
+      )}
+    </Dropzone>
   )
 }
 
