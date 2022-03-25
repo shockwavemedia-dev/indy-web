e6c020b56ad90a6ac5c7d8d3e21b38111d509772
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-25 09:07:06 +0800

    Return label prop to file input

--- components/Common/FileInput.component.tsx
+++ components/Common/FileInput.component.tsx
@@ -1,11 +1,13 @@
 import Dropzone from 'react-dropzone'
 
 const FileInput = ({
+  label,
   name,
   setFieldValue,
   multiple = false,
   className,
 }: {
+  label: string
   name: string
   setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void
   multiple?: boolean
@@ -16,31 +18,34 @@ const FileInput = ({
   return (
     <Dropzone onDrop={dropFiles} noClick noKeyboard multiple={multiple}>
       {({ getRootProps, getInputProps, open, isDragActive }) => (
-        <div
-          {...getRootProps()}
-          className={`flex min-h-35 cursor-auto items-center justify-center overflow-hidden rounded-xl border border-dashed border-lavender-gray ${
-            isDragActive ? 'bg-honeydew' : 'bg-ghost-white'
-          } ${className}`}
-        >
-          <div className="cursor absolute flex flex-col items-center">
-            <div className="mb-1.5 font-urbanist text-sm font-medium text-onyx">
-              Drag &amp; drop you assets, or&nbsp;
-              <button
-                className="font-semibold text-jungle-green underline underline-offset-1"
-                name="Browse Assets"
-                onClick={open}
-                type="button"
-              >
-                browse it
-              </button>
+        <>
+          <div className="mb-2 font-urbanist text-base font-medium text-onyx">{label}</div>
+          <div
+            {...getRootProps()}
+            className={`flex min-h-35 cursor-auto items-center justify-center overflow-hidden rounded-xl border border-dashed border-lavender-gray ${
+              isDragActive ? 'bg-honeydew' : 'bg-ghost-white'
+            } ${className}`}
+          >
+            <div className="cursor absolute flex flex-col items-center">
+              <div className="mb-1.5 font-urbanist text-sm font-medium text-onyx">
+                Drag &amp; drop you assets, or&nbsp;
+                <button
+                  className="font-semibold text-jungle-green underline underline-offset-1"
+                  name="Browse Assets"
+                  onClick={open}
+                  type="button"
+                >
+                  browse it
+                </button>
+              </div>
+              <div className="text-center font-urbanist text-xs font-medium text-metallic-silver">
+                Files Supported: MP3, MP4, AVI, MOV, FLV. <br />
+                Maximum size: 250 MB
+              </div>
             </div>
-            <div className="text-center font-urbanist text-xs font-medium text-metallic-silver">
-              Files Supported: MP3, MP4, AVI, MOV, FLV. <br />
-              Maximum size: 250 MB
-            </div>
+            <input {...getInputProps()} />
           </div>
-          <input {...getInputProps()} />
-        </div>
+        </>
       )}
     </Dropzone>
   )
