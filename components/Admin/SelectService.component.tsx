ae75604964f598e39b0071e902f9ea250e0b2a17
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-29 09:48:23 +0800

    Fix api path

--- components/Admin/SelectService.component.tsx
+++ components/Admin/SelectService.component.tsx
@@ -16,7 +16,7 @@ const SelectService = ({
 
   const { data: services, isLoading } = useQuery('services', async () => {
     const { data } = await axios.get<Array<Service>>(
-      `/clients/${session?.user.userType.clientId}/services`,
+      `/v1/clients/${session?.user.userType.clientId}/services`,
       {
         headers: {
           Authorization: `Bearer ${session?.accessToken}`,
