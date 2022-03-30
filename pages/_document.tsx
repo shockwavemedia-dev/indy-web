73bc8bb7ea7d7bc5df50a0e80830164043b83423
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-31 07:45:40 +0800

    Add bold font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ const Document = () => (
         rel="stylesheet"
       />
       <link
-        href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap"
         rel="stylesheet"
       />
       <link rel="icon" href="/favicon.ico" />
