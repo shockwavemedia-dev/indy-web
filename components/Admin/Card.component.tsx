3de301b911a8cc6e96f53d8181ff45c0ebc85785
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-03-31 09:21:29 +0800

    Update card component

--- components/Admin/Card.component.tsx
+++ components/Admin/Card.component.tsx
@@ -9,8 +9,10 @@ const Card = ({
   children: ReactNode
   className?: string
 }) => (
-  <div className={`flex flex-col rounded-xl bg-white p-6 shadow ${className}`}>
-    <div className="mb-5 font-urbanist text-base font-semibold text-onyx">{title}</div>
+  <div className={`relative flex flex-col rounded-xl bg-white p-6 shadow ${className}`}>
+    <div className="absolute top-6 left-6 font-urbanist text-base font-semibold text-onyx">
+      {title}
+    </div>
     {children}
   </div>
 )
