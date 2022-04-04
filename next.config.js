285b1adc401e62fcb00d56139d510ac99428a74d
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-04-05 07:50:37 +0800

    Remove unused icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: true,
+})
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
