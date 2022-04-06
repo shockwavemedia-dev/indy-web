bf50e40294c12a91b8f6f3fff985c40e8f39f1cb
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-04-06 08:45:20 +0800

    update validation in textInput and textArea

--- components/Common/TextAreaInput.component.tsx
+++ components/Common/TextAreaInput.component.tsx
@@ -19,14 +19,18 @@ const TextAreaInput = ({
   <div className={`block ${className}`}>
     <Icon className="pointer-events-none absolute ml-6 mt-4 stroke-lavender-gray" />
     <Field
-      className="no-sc min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+      className={
+        touched && errorMessage
+          ? 'no-sc min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-fire-brick selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40'
+          : 'no-sc min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40'
+      }
       component="textarea"
       name={name}
       spellCheck={false}
       placeholder={placeholder}
     />
     {touched && errorMessage && (
-      <div className="font-small mb-3 mt-0 w-full whitespace-pre-line font-urbanist text-xs capitalize  text-fire-brick">
+      <div className="text-rose-900 mb-3 mt-0 w-full whitespace-pre-line font-urbanist text-xxs font-semibold capitalize  text-fire-brick">
         {touched && errorMessage}
       </div>
     )}
