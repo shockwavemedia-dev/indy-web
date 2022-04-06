dfb81ec5cb9eaf8b32536b5a2fca7f680fd9fe83
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-04-06 15:10:45 +0800

    Display actions in tickets table

--- constants/TicketTableColumns.tsx
+++ constants/TicketTableColumns.tsx
@@ -1,5 +1,7 @@
 import { format } from 'date-fns'
 import { Column } from 'react-table'
+import EyeOpenIcon from '../components/Common/Icons/EyeOpen.icon'
+import Link from '../components/Common/Link.component'
 import { Ticket } from '../interfaces/Ticket.interface'
 
 export const TicketTableColumns: Array<Column<Ticket>> = [
@@ -34,6 +36,16 @@ export const TicketTableColumns: Array<Column<Ticket>> = [
       <div className="font-urbanist text-sm font-medium text-onyx">
         {format(value, "yy MMM''dd")}
       </div>
+    ),
+  },
+  {
+    Header: 'Actions',
+    accessor: 'id',
+    disableSortBy: true,
+    Cell: ({ value }) => (
+      <Link href={`/ticket/${value}`}>
+        <EyeOpenIcon className="stroke-waterloo" />
+      </Link>
     ),
   },
 ]
--- interfaces/Ticket.interface.ts
+++ interfaces/Ticket.interface.ts
@@ -1,6 +1,9 @@
+import { Service } from './Service.interface'
+
 export interface Ticket {
   id: number
   ticketCode: string
+  clientId?: number
   subject: string
   description: string
   departmentName: string
@@ -9,4 +12,5 @@ export interface Ticket {
   status: 'closed' | 'new' | 'pending' | 'on_hold' | 'open' | 'resolved' | 'deleted'
   createdAt: Date
   attachment: string
+  services?: Array<Service>
 }
