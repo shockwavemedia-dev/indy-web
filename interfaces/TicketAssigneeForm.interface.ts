b3cc8a5119dce06a5dba17f9e9573d3f868cde0b
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-04-07 14:55:02 +0800

    ticket assignee datatable with delete

--- interfaces/TicketAssigneeForm.interface.ts
+++ interfaces/TicketAssigneeForm.interface.ts
@@ -1,4 +1,9 @@
 export interface TicketAssigneeForm {
   departmentId: number
+  departmentName: string
   adminUserId: number
+  role: 'account_manager' | 'admin' | 'manager' | 'staff'
+  fullName: string
+  ticketAssigneeId: number
+  status: string
 }
