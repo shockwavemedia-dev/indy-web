1a036ce4be03ca0dd80d016f276742a84276e82b
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-04-13 06:30:38 +0800

    Fix imports

--- components/Panel/NewAdminUserModal.tsx
+++ components/Panel/NewAdminUserModal.tsx
@@ -2,39 +2,29 @@ import axios from 'axios'
 import { Form, Formik } from 'formik'
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
-import { useQuery, useQueryClient } from 'react-query'
-import { ClientUserRoleOptions } from '../../constants/ClientUserRoleOptions'
+import { useQueryClient } from 'react-query'
+import { AdminUserRoleOptions } from '../../constants/AdminUserRoleOptions'
 import { UserGenderOptions } from '../../constants/UserGenderOptions'
-import { Client } from '../../interfaces/Client.interface'
-import { NewClientUserForm } from '../../interfaces/NewClientUserForm.interface'
-import { Page } from '../../interfaces/Page.interface'
-import { NewClientUserFormSchema } from '../../schemas/NewClientUserFormSchema'
-import {
-  computePasswordStrength,
-  PasswordStrengthMeter,
-} from '../Auth/PasswordStrengthMeter.component'
-import Button from '../Common/Button.component'
-import DateInput from '../Common/DateInput.component'
-import BriefcaseIcon from '../Common/Icons/Briefcase.icon'
-import ClipboardIcon from '../Common/Icons/Clipboard.icon'
-import EmailIcon from '../Common/Icons/Email.icon'
-import LockIcon from '../Common/Icons/Lock.icon'
-import PencilIcon from '../Common/Icons/Pencil.icon'
-import UserIcon from '../Common/Icons/User.icon'
-import Select from '../Common/Select.component'
-import TextInput from '../Common/TextInput.component'
-import Modal from './Modal.component'
-const NewClientUserModal = ({
-  isVisible,
-  onClose,
-}: {
-  isVisible: boolean
-  onClose: () => void
-}) => {
+import { NewAdminUserForm } from '../../interfaces/NewAdminUserForm.interface'
+import { NewAdminUserFormSchema } from '../../schemas/NewAdminUserFormSchema'
+import Button from '../common/Button'
+import DateInput from '../common/DateInput'
+import ClipboardIcon from '../common/icons/ClipboardIcon'
+import EmailIcon from '../common/icons/EmailIcon'
+import LockIcon from '../common/icons/LockIcon'
+import PencilIcon from '../common/icons/PencilIcon'
+import UserIcon from '../common/icons/UserIcon'
+import PasswordInput from '../common/PasswordInput'
+import { computePasswordStrength, PasswordStrengthMeter } from '../common/PasswordStrengthMeter'
+import Select from '../common/Select'
+import TextInput from '../common/TextInput'
+import Modal from './Modal'
+
+const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose: () => void }) => {
   const { data: session } = useSession()
   const queryClient = useQueryClient()
 
-  const formInitialValues: NewClientUserForm = {
+  const formInitialValues: NewAdminUserForm = {
     email: '',
     password: '',
     birthDate: '',
@@ -45,7 +35,6 @@ const NewClientUserModal = ({
     middleName: '',
     gender: null,
     role: null,
-    clientId: 0,
   }
 
   const [passwordStrength, setPasswordStrength] = useState(0)
@@ -53,35 +42,13 @@ const NewClientUserModal = ({
   const updatePasswordStrength = (password: string) =>
     setPasswordStrength(computePasswordStrength(password))
 
-  const { data: clients } = useQuery('clients', async () => {
-    const {
-      data: { data },
-    } = await axios.get<{
-      data: Array<Client>
-      page: Page
-    }>('/v1/clients', {
-      headers: {
-        Authorization: `Bearer ${session?.accessToken}`,
-      },
-    })
-
-    return data
-  })
-
-  const clientOptions = clients
-    ?.filter((client) => client.status === 'active')
-    .map(({ id, name }) => ({
-      value: id,
-      label: name,
-    }))
-
   const submitForm = async (
-    values: NewClientUserForm,
+    values: NewAdminUserForm,
     { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
   ) => {
     setSubmitting(true)
 
-    const { status } = await axios.post('/v1/users/client', values, {
+    const { status } = await axios.post('/v1/users/admin', values, {
       headers: {
         Authorization: `Bearer ${session?.accessToken}`,
       },
@@ -100,9 +67,9 @@ const NewClientUserModal = ({
   return (
     <>
       {isVisible && (
-        <Modal title="New Client User" onClose={onClose}>
+        <Modal title="New Admin User" onClose={onClose}>
           <Formik
-            validationSchema={NewClientUserFormSchema}
+            validationSchema={NewAdminUserFormSchema}
             initialValues={formInitialValues}
             onSubmit={submitForm}
             validate={validateForm}
@@ -110,40 +77,32 @@ const NewClientUserModal = ({
             {({ isSubmitting, setFieldValue }) => (
               <Form className="flex w-140 flex-col">
                 <Select
-                  name="clientId"
-                  Icon={BriefcaseIcon}
-                  placeholder="Select Client"
-                  options={clientOptions || []}
-                  setFieldValue={setFieldValue}
-                  className="mb-5"
-                />
-                <Select
                   name="role"
                   Icon={ClipboardIcon}
                   placeholder="Select Role"
-                  options={ClientUserRoleOptions}
+                  options={AdminUserRoleOptions}
                   setFieldValue={setFieldValue}
                   className="mb-5"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter First Name"
                   name="firstName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter Middle Name"
                   name="middleName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter Last Name"
                   name="lastName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <Select
@@ -159,36 +118,27 @@ const NewClientUserModal = ({
                     name="birthDate"
                     placeholder="Enter Birth Date"
                     setFieldValue={setFieldValue}
-                    className="mb-5"
                   />
                   <TextInput
+                    type="text"
                     Icon={PencilIcon}
                     placeholder="Enter Phone"
                     name="contactNumber"
-                    disableAutoComplete
                   />
                 </div>
                 <TextInput
+                  type="email"
                   Icon={EmailIcon}
                   placeholder="Enter Email"
                   name="email"
-                  disableAutoComplete
                   className="mb-5"
                 />
                 <div className="mb-3 flex w-full space-x-5">
-                  <TextInput
-                    type="password"
-                    name="password"
-                    Icon={LockIcon}
-                    placeholder="Enter password"
-                    disableAutoComplete
-                  />
-                  <TextInput
-                    type="password"
+                  <PasswordInput name="password" Icon={LockIcon} placeholder="Enter password" />
+                  <PasswordInput
                     name="passwordConfirmation"
                     Icon={LockIcon}
                     placeholder="Confirm password"
-                    disableAutoComplete
                   />
                 </div>
                 <PasswordStrengthMeter strength={passwordStrength} className="mr-auto mb-2" />
@@ -201,7 +151,7 @@ const NewClientUserModal = ({
                   <Button ariaLabel="Cancel" light onClick={onClose}>
                     Cancel
                   </Button>
-                  <Button ariaLabel="Submit" type="submit" disabled={isSubmitting}>
+                  <Button ariaLabel="Submit" disabled={isSubmitting} submit>
                     Submit
                   </Button>
                 </div>
@@ -214,4 +164,4 @@ const NewClientUserModal = ({
   )
 }
 
-export default NewClientUserModal
+export default NewAdminUserModal
--- components/Panel/NewClientUserModal.tsx
+++ components/Panel/NewClientUserModal.tsx
@@ -2,31 +2,38 @@ import axios from 'axios'
 import { Form, Formik } from 'formik'
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
-import { useQueryClient } from 'react-query'
-import { AdminUserRoleOptions } from '../../constants/AdminUserRoleOptions'
+import { useQuery, useQueryClient } from 'react-query'
+import { ClientUserRoleOptions } from '../../constants/ClientUserRoleOptions'
 import { UserGenderOptions } from '../../constants/UserGenderOptions'
-import { NewAdminUserForm } from '../../interfaces/NewAdminUserForm.interface'
-import { NewAdminUserFormSchema } from '../../schemas/NewAdminUserFormSchema'
-import {
-  computePasswordStrength,
-  PasswordStrengthMeter,
-} from '../Auth/PasswordStrengthMeter.component'
-import Button from '../Common/Button.component'
-import DateInput from '../Common/DateInput.component'
-import ClipboardIcon from '../Common/Icons/Clipboard.icon'
-import EmailIcon from '../Common/Icons/Email.icon'
-import LockIcon from '../Common/Icons/Lock.icon'
-import PencilIcon from '../Common/Icons/Pencil.icon'
-import UserIcon from '../Common/Icons/User.icon'
-import Select from '../Common/Select.component'
-import TextInput from '../Common/TextInput.component'
-import Modal from './Modal.component'
+import { NewClientUserForm } from '../../interfaces/NewClientUserForm.interface'
+import { NewClientUserFormSchema } from '../../schemas/NewClientUserFormSchema'
+import { Client } from '../../types/Client.type'
+import { Page } from '../../types/Page.type'
+import Button from '../common/Button'
+import DateInput from '../common/DateInput'
+import BriefcaseIcon from '../common/icons/BriefcaseIcon'
+import ClipboardIcon from '../common/icons/ClipboardIcon'
+import EmailIcon from '../common/icons/EmailIcon'
+import LockIcon from '../common/icons/LockIcon'
+import PencilIcon from '../common/icons/PencilIcon'
+import UserIcon from '../common/icons/UserIcon'
+import PasswordInput from '../common/PasswordInput'
+import { computePasswordStrength, PasswordStrengthMeter } from '../common/PasswordStrengthMeter'
+import Select from '../common/Select'
+import TextInput from '../common/TextInput'
+import Modal from './Modal'
 
-const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose: () => void }) => {
+const NewClientUserModal = ({
+  isVisible,
+  onClose,
+}: {
+  isVisible: boolean
+  onClose: () => void
+}) => {
   const { data: session } = useSession()
   const queryClient = useQueryClient()
 
-  const formInitialValues: NewAdminUserForm = {
+  const formInitialValues: NewClientUserForm = {
     email: '',
     password: '',
     birthDate: '',
@@ -37,6 +44,7 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
     middleName: '',
     gender: null,
     role: null,
+    clientId: 0,
   }
 
   const [passwordStrength, setPasswordStrength] = useState(0)
@@ -44,13 +52,35 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
   const updatePasswordStrength = (password: string) =>
     setPasswordStrength(computePasswordStrength(password))
 
+  const { data: clients } = useQuery('clients', async () => {
+    const {
+      data: { data },
+    } = await axios.get<{
+      data: Array<Client>
+      page: Page
+    }>('/v1/clients', {
+      headers: {
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    })
+
+    return data
+  })
+
+  const clientOptions = clients
+    ?.filter((client) => client.status === 'active')
+    .map(({ id, name }) => ({
+      value: id,
+      label: name,
+    }))
+
   const submitForm = async (
-    values: NewAdminUserForm,
+    values: NewClientUserForm,
     { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
   ) => {
     setSubmitting(true)
 
-    const { status } = await axios.post('/v1/users/admin', values, {
+    const { status } = await axios.post('/v1/users/client', values, {
       headers: {
         Authorization: `Bearer ${session?.accessToken}`,
       },
@@ -69,9 +99,9 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
   return (
     <>
       {isVisible && (
-        <Modal title="New Admin User" onClose={onClose}>
+        <Modal title="New Client User" onClose={onClose}>
           <Formik
-            validationSchema={NewAdminUserFormSchema}
+            validationSchema={NewClientUserFormSchema}
             initialValues={formInitialValues}
             onSubmit={submitForm}
             validate={validateForm}
@@ -79,32 +109,40 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
             {({ isSubmitting, setFieldValue }) => (
               <Form className="flex w-140 flex-col">
                 <Select
+                  name="clientId"
+                  Icon={BriefcaseIcon}
+                  placeholder="Select Client"
+                  options={clientOptions || []}
+                  setFieldValue={setFieldValue}
+                  className="mb-5"
+                />
+                <Select
                   name="role"
                   Icon={ClipboardIcon}
                   placeholder="Select Role"
-                  options={AdminUserRoleOptions}
+                  options={ClientUserRoleOptions}
                   setFieldValue={setFieldValue}
                   className="mb-5"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter First Name"
                   name="firstName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter Middle Name"
                   name="middleName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <TextInput
+                  type="text"
                   Icon={UserIcon}
                   placeholder="Enter Last Name"
                   name="lastName"
-                  disableAutoComplete
                   className="mb-8"
                 />
                 <Select
@@ -123,33 +161,25 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
                     className="mb-5"
                   />
                   <TextInput
+                    type="text"
                     Icon={PencilIcon}
                     placeholder="Enter Phone"
                     name="contactNumber"
-                    disableAutoComplete
                   />
                 </div>
                 <TextInput
+                  type="email"
                   Icon={EmailIcon}
                   placeholder="Enter Email"
                   name="email"
-                  disableAutoComplete
                   className="mb-5"
                 />
                 <div className="mb-3 flex w-full space-x-5">
-                  <TextInput
-                    type="password"
-                    name="password"
-                    Icon={LockIcon}
-                    placeholder="Enter password"
-                    disableAutoComplete
-                  />
-                  <TextInput
-                    type="password"
+                  <PasswordInput name="password" Icon={LockIcon} placeholder="Enter password" />
+                  <PasswordInput
                     name="passwordConfirmation"
                     Icon={LockIcon}
                     placeholder="Confirm password"
-                    disableAutoComplete
                   />
                 </div>
                 <PasswordStrengthMeter strength={passwordStrength} className="mr-auto mb-2" />
@@ -162,7 +192,7 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
                   <Button ariaLabel="Cancel" light onClick={onClose}>
                     Cancel
                   </Button>
-                  <Button ariaLabel="Submit" type="submit" disabled={isSubmitting}>
+                  <Button ariaLabel="Submit" disabled={isSubmitting} submit>
                     Submit
                   </Button>
                 </div>
@@ -175,4 +205,4 @@ const NewAdminUserModal = ({ isVisible, onClose }: { isVisible: boolean; onClose
   )
 }
 
-export default NewAdminUserModal
+export default NewClientUserModal
