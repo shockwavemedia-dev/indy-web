cb7b96850520f62a85f9c6f9abfd28ec48d61a14
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-04-13 08:58:30 +0800

    Refactor text area input

--- components/Common/TextAreaInput.tsx
+++ components/Common/TextAreaInput.tsx
@@ -12,20 +12,16 @@ const TextAreaInput = ({
   Icon: Icon
   placeholder: string
   className?: string
-  errorMessage?: string
-  touched?: boolean
 }) => (
   <div className={className}>
-    <div className="flex">
-      <Icon className="pointer-events-none absolute ml-6 mt-4 stroke-lavender-gray" />
-      <Field
-        className="min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
-        component="textarea"
-        name={name}
-        spellCheck={false}
-        placeholder={placeholder}
-      />
-    </div>
+    <Icon className="pointer-events-none absolute ml-6 mt-4 stroke-lavender-gray" />
+    <Field
+      className="min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+      component="textarea"
+      name={name}
+      spellCheck={false}
+      placeholder={placeholder}
+    />
     <FormErrorMessage name={name} />
   </div>
 )
