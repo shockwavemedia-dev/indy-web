99b497f42d3ca51ae2192508c10d4e9d9b8a6361
Kyle Oliva <Kyle.Oliva@dailypress.com.au>
2022-04-13 09:02:02 +0800

    Refactor text area input

--- components/Common/TextAreaInput.tsx
+++ components/Common/TextAreaInput.tsx
@@ -16,7 +16,7 @@ const TextAreaInput = ({
   <div className={className}>
     <Icon className="pointer-events-none absolute ml-6 mt-4 stroke-lavender-gray" />
     <Field
-      className="min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
+      className="-mb-1 min-h-35 w-full resize-none overflow-hidden rounded-xl bg-transparent py-4 px-13 font-urbanist text-sm font-medium text-onyx placeholder-metallic-silver ring-1 ring-bright-gray selection:bg-jungle-green selection:text-white focus:ring-2 focus:ring-jungle-green focus:ring-opacity-40"
       component="textarea"
       name={name}
       spellCheck={false}
