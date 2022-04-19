4043575a732de141d459d2495ef289ed7e3ce9d6
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-04-20 07:41:41 +0800

    update delete ticket assignee

--- components/panel/modals/TicketAssigneeDeleteModal.tsx
+++ components/panel/modals/TicketAssigneeDeleteModal.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useSession } from 'next-auth/react'
 import { useQuery, useQueryClient } from 'react-query'
 import { TicketAssigneeForm } from '../../../types/forms/TicketAssigneeForm.type'
-import Button from '../../Common/Button'
+import Button from '../../common/Button'
 import Modal from '../Modal'
 
 const TicketAssigneeDeleteModal = ({
@@ -49,7 +49,7 @@ const TicketAssigneeDeleteModal = ({
         <Modal title="Are you sure you want to delete Ticket Assignee?" onClose={onClose}>
           <div className="mb-8 flex w-140 flex-col">
             <div className="grid grid-cols-2 grid-rows-3 gap-6">
-              <div className="col-span-2 space-y-1">
+              <div className="space-y-1">
                 <div
                   className={`font-urbanist text-sm font-bold text-onyx ${
                     isLoading && 'w-20 animate-pulse rounded bg-metallic-silver text-transparent'
@@ -66,7 +66,7 @@ const TicketAssigneeDeleteModal = ({
                   {!isLoading && ticketAssignee?.fullName}
                 </div>
               </div>
-              <div className="col-span-2 space-y-1">
+              <div className="space-y-1">
                 <div
                   className={`font-urbanist text-sm font-bold text-onyx ${
                     isLoading && 'w-20 animate-pulse rounded bg-metallic-silver text-transparent'
@@ -83,7 +83,7 @@ const TicketAssigneeDeleteModal = ({
                   {!isLoading && ticketAssignee?.departmentName}
                 </div>
               </div>
-              <div className="col-span-2 space-y-1">
+              <div className="space-y-1">
                 <div
                   className={`font-urbanist text-sm font-bold text-onyx ${
                     isLoading && 'w-20 animate-pulse rounded bg-metallic-silver text-transparent'
@@ -100,7 +100,7 @@ const TicketAssigneeDeleteModal = ({
                   {!isLoading && ticketAssignee?.role}
                 </div>
               </div>
-              <div className="col-span-2 space-y-1">
+              <div className="space-y-1">
                 <div
                   className={`font-urbanist text-sm font-bold text-onyx ${
                     isLoading && 'w-20 animate-pulse rounded bg-metallic-silver text-transparent'
@@ -118,7 +118,7 @@ const TicketAssigneeDeleteModal = ({
                 </div>
               </div>
             </div>
-            <div className="mt-10 flex space-x-5">
+            <div className="flex space-x-5">
               <Button ariaLabel="Cancel" onClick={onClose} type="button" light>
                 Cancel
               </Button>
