3f42cb799c20cdaefbe91854638ecf37bdaf3b99
Kyle Oliva <kyle.oliva@dailypress.com.au>
2022-05-04 09:46:33 +0800

    Rename form

---
+++ schemas/CreateEventFormSchema.ts
@@ -0,0 +1,21 @@
+import { array, date, mixed, number, object, SchemaOf, string } from 'yup'
+import { CreateEventForm } from '../types/forms/CreateEventForm.type'
+
+export const CreateEventFormSchema: SchemaOf<CreateEventForm> = object().shape({
+  requestedBy: number().required(),
+  clientId: number().required(),
+  subject: string().required(),
+  services: array()
+    .of(
+      object()
+        .shape({
+          serviceId: number().required(),
+          extras: array().of(string().required()),
+        })
+        .required()
+    )
+    .min(1),
+  duedate: date().required().nullable(),
+  description: string().required(),
+  attachment: mixed().optional(),
+})
---
+++ types/forms/CreateEventForm.type.ts
@@ -0,0 +1,9 @@
+export type CreateEventForm = {
+  requestedBy: number
+  clientId: number
+  subject: string
+  services: Array<{ serviceId: number; extras: Array<string> }>
+  duedate: Date | null
+  description: string
+  attachment?: File
+}
