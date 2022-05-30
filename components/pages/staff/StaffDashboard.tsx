daa7df7032c71b79788b7f98adcce3debffb3fd0
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-05-30 08:39:14 +0800

    update manager and staff dashboard

--- components/pages/staff/StaffDashboard.tsx
+++ components/pages/staff/StaffDashboard.tsx
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import { StaffTicketsTableColumns } from '../../../constants/tables/StaffTicketsTableColumns'
 import { Card } from '../../Card'
-import { CountCard } from '../../CountCard'
 import { DataTable } from '../../DataTable'
-import { GalleryIcon } from '../../icons/GalleryIcon'
-import { MenuBoardIcon } from '../../icons/MenuBoardIcon'
-import { VideoIcon } from '../../icons/VideoIcon'
 
 export const StaffDashboard = () => {
   return (
@@ -16,7 +12,7 @@ export const StaffDashboard = () => {
       <div className="mb-5 font-urbanist text-xxl font-semibold text-onyx">Dashboard</div>
       <div className="mx-auto flex h-full w-full max-w-7xl flex-col">
         <div className="mb-6 flex h-155 space-x-6">
-          <Card title="My Tickets" className="flex w-228 flex-col">
+          <Card title="My Tickets" className="flex w-full flex-col">
             <DataTable
               columns={StaffTicketsTableColumns}
               dataEndpoint="/v1/my-tickets"
@@ -25,61 +21,6 @@ export const StaffDashboard = () => {
               settings
               periodicFilter
             />
-          </Card>
-          <div className="flex flex-1 flex-col">
-            <div className="mb-3 flex space-x-3">
-              <CountCard
-                Icon={
-                  <div className="grid h-11.5 w-11.5 flex-none place-items-center rounded-lg bg-cosmic-latte">
-                    <VideoIcon className="stroke-deep-saffron" />
-                  </div>
-                }
-                value={5}
-                description="Animations Remaining"
-                className="w-36"
-              />
-              <CountCard
-                Icon={
-                  <div className="grid h-11.5 w-11.5 flex-none place-items-center rounded-lg bg-honeydew">
-                    <GalleryIcon className="stroke-jungle-green" />
-                  </div>
-                }
-                value={2}
-                description="Photoshoots Remaining"
-              />
-            </div>
-            <div className="mb-6 flex space-x-3">
-              <CountCard
-                Icon={
-                  <div className="grid h-11.5 w-11.5 flex-none place-items-center rounded-lg bg-alice-blue">
-                    <MenuBoardIcon className="stroke-bleu-de-france" />
-                  </div>
-                }
-                value={3}
-                description="Marketing Strategy Remaining"
-              />
-              <CountCard
-                Icon={
-                  <div className="grid h-11.5 w-11.5 flex-none place-items-center rounded-lg bg-magnolia">
-                    <VideoIcon className="stroke-purple-x11" />
-                  </div>
-                }
-                value={1}
-                description="Videoshoots Remaining"
-                className="w-36"
-              />
-            </div>
-            <Card title="Notifications" className="h-full w-full opacity-50">
-              <div></div>
-            </Card>
-          </div>
-        </div>
-        <div className="flex h-102 space-x-6">
-          <Card title="Number of Projects" className="w-228 opacity-50">
-            <></>
-          </Card>
-          <Card title="Requests" className="flex-1 opacity-50">
-            <></>
           </Card>
         </div>
       </div>
