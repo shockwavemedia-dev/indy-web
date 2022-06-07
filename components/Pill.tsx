fb7f6fd2d8ab72d09d9ec03911a480b7cb34c0d7
Kyle Oliva <kyle.oliva@dailypress.com.au>
2022-06-07 08:33:02 +0800

    Remove border when pill color is present

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -10,7 +10,9 @@ export const Pill = ({
   twTextColor?: string
 }) => (
   <div
-    className={`${twBackgroundColor} flex h-6 w-fit items-center space-x-1.5 rounded-lg border border-bright-gray px-2.5`}
+    className={`${
+      twBackgroundColor ?? 'border'
+    }  flex h-6 w-fit items-center space-x-1.5 rounded-lg border-bright-gray px-2.5`}
   >
     {pillColor && <div className={`h-1.5 w-1.5 rounded-full ${pillColor}`} />}
     <div className={`${twTextColor} font-urbanist text-sm font-medium capitalize`}>{value}</div>
