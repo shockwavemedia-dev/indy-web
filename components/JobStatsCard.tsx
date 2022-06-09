2d34bd1f1b2abff1d1b841522a0b7a8a6955e7e7
Kyle Oliva <kyle.oliva@dailypress.com.au>
2022-06-09 08:05:00 +0800

    Update navigation bar colors

--- components/JobStatsCard.tsx
+++ components/JobStatsCard.tsx
@@ -9,9 +9,9 @@ export const JobsStatusCountCard = ({
   value: number
   description: string
 }) => (
-  <div className="flex h-30 w-full flex-col rounded-xl bg-deep-saffron py-4.5 pl-5">
-    <div className="mb-2.5 grid h-9.5 w-9.5 flex-none place-items-center rounded-md bg-carrot-orange">
-      <Icon className="stroke-white" />
+  <div className="flex h-30 w-full flex-col rounded-xl bg-quartz py-4.5 pl-5">
+    <div className="mb-2.5 grid h-9.5 w-9.5 flex-none place-items-center rounded-md bg-charleston-green">
+      <Icon className="stroke-halloween-orange" />
     </div>
     <div className="mb-1 font-urbanist text-sm font-semibold text-white">{value}</div>
     <div className="font-urbanist text-xs font-medium text-white">{description}</div>
