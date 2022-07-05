f50d1d832fa526a2a38de490ae750fec5f17bf7a
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-07-05 12:47:16 +0800

    add retainer inclusions in client dashboard

---
+++ components/StatusCountCard.tsx
@@ -0,0 +1,18 @@
+export const StatusCountCard = ({
+  value,
+  description,
+  className,
+}: {
+  value: number
+  description: string
+  className?: string
+}) => (
+  <div
+    className={`flex h-22 w-22 flex-col items-center justify-center rounded-xl py-2 ${className}`}
+  >
+    <div className="mb-1 font-urbanist text-xl font-semibold text-white">{value}</div>
+    <div className="mx-1 text-center font-urbanist text-xs font-medium text-white">
+      {description}
+    </div>
+  </div>
+)
