9539de6acb29df7606b8dba22ef742529e381bb6
Kyle Oliva <kyle.oliva@dailypress.com.au>
2022-07-19 10:02:56 +0800

    Improve axios date transformer

--- utils/DateHelpers.ts
+++ utils/DateHelpers.ts
@@ -7,7 +7,11 @@ export function parseDates(data: Record<string, unknown>) {
     const value = data[key]
 
     if (value && typeof value === 'string' && /\d{4}-\d{2}-\d{2}/.test(value)) {
-      data[key] = new Date(value)
+      const date = new Date(value)
+
+      if (!isNaN(date.getTime())) {
+        data[key] = date
+      }
     } else if (typeof value === 'object') {
       parseDates(value as Record<string, unknown>)
     }
