3f6fd41deba156f56def7ca12eba9d180baf414c
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-08-01 09:36:52 +0800

    website page with this list of layouts

---
+++ constants/options/WebsiteServicesList.ts
@@ -0,0 +1,19 @@
+import Atmospherevatar from '../../public/images/website-atmosphere-avatar.png'
+import CalypsoAvatar from '../../public/images/website-calypso-avatar.png'
+import CherryAvatar from '../../public/images/website-cherry-avatar.png'
+import DestinationAvatar from '../../public/images/website-destination-avatar.png'
+import ElementAvatar from '../../public/images/website-element-avatar.png'
+import FynbossAvatar from '../../public/images/website-fynboss-avatar.png'
+import PlaturAvatar from '../../public/images/website-platur-avatar.png'
+import TerraceAvatar from '../../public/images/website-terrace-avatar.png'
+
+export const WebsiteServicesList = [
+  { image: Atmospherevatar, link: 'https://atmospherestg.wpengine.com' },
+  { image: CalypsoAvatar, link: 'https://calypsodesistg.wpengine.com' },
+  { image: CherryAvatar, link: 'https://cherrylane1stg.wpengine.com' },
+  { image: DestinationAvatar, link: 'https://cherrylane1stg.wpengine.com' },
+  { image: ElementAvatar, link: 'https://elementstg.wpengine.com' },
+  { image: FynbossAvatar, link: 'https://fynbosstg.wpengine.com' },
+  { image: PlaturAvatar, link: 'https://platurdesigstg.wpengine.com' },
+  { image: TerraceAvatar, link: 'https://terracestg.wpengine.com/' },
+]
--- pages/website-services.tsx
+++ pages/website-services.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { ReactElement, useEffect } from 'react'
 import { Card } from '../components/Card'
 import { Notifications } from '../components/Notifications'
 import { RetainerInclusions } from '../components/RetainerInclusions'
+import { WebsiteServicesList } from '../constants/options/WebsiteServicesList'
 import PanelLayout, { usePanelLayoutStore } from '../layouts/PanelLayout'
 import { NextPageWithLayout } from '../types/pages/NextPageWithLayout.type'
 
@@ -20,8 +22,20 @@ const WebsitePage: NextPageWithLayout = () => {
       </Head>
       <div className="mx-auto w-full max-w-8xl space-y-6">
         <div className="flex gap-6 transition-all lg:flex-col">
-          <Card className="grid h-155 flex-1 place-items-center transition-all lg:flex-none">
-            <div className="">Nothing to see here. 🦗</div>
+          <Card className="grid h-fit w-full flex-1 place-items-center transition-all lg:flex-none">
+            <div className="flex w-212.5 flex-wrap place-content-center gap-2">
+              {WebsiteServicesList?.map(({ image, link }) => (
+                <a key={`${link}-website`} href={link} target="_blank" rel="noopener noreferrer">
+                  <Image
+                    src={image}
+                    alt="website-atmosphere"
+                    height={400}
+                    width={400}
+                    className="rounded-xl"
+                  />
+                </a>
+              ))}
+            </div>
           </Card>
           <div className="flex w-86 flex-col gap-6 transition-all lg:w-full lg:flex-row">
             <RetainerInclusions />
