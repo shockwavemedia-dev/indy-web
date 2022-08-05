04af0737535d4a640148c535b2a34cfe52abd15e
Mark <mark.reyta@dailypress.com.au>
2022-08-05 13:12:27 +0800

    - bring back to dev api

--- constants/Http.ts
+++ constants/Http.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.BASE_URL || 'https://crm-api-testing.herokuapp.com/api'
+export const API_BASE_URL = process.env.BASE_URL || 'https://crm-api-v1.herokuapp.com/api'
