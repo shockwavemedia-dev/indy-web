fee24706a9eb691b6dbbe02c90b22b79929b2022
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-08-08 09:12:43 +0800

    add download function

--- components/modals/ImageFileDisplayModal.tsx
+++ components/modals/ImageFileDisplayModal.tsx
@@ -2,6 +2,9 @@ import Image, { StaticImageData } from 'next/image'
 import createStore from 'zustand'
 import { combine } from 'zustand/middleware'
 import DummyFile from '../../public/images/dummy-file.png'
+import { Button } from '../Button'
+import { DownloadIcon } from '../icons/DownloadIcon'
+import { PrintIcon } from '../icons/PrintIcon'
 import { Modal } from '../Modal'
 
 export const useImageFileDisplayModalStore = createStore(
@@ -20,12 +23,37 @@ export const useImageFileDisplayModalStore = createStore(
 export const ImageFileDisplayModal = () => {
   const { signedUrl, name, toggleShowImageFileModal } = useImageFileDisplayModalStore()
 
+  const downloadFile = () => {
+    const link = document.createElement('a')
+    link.href = '/images/visual-display/' + name
+    link.setAttribute('download', name)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
   return (
     <>
       {signedUrl && name && (
         <Modal title={name} onClose={toggleShowImageFileModal}>
           <div className="max-h-155 w-fit overflow-hidden rounded-xl">
             <Image src={signedUrl} alt={name} className="h-full w-full" />
+            <div className="mt-5 flex space-x-5">
+              <Button
+                ariaLabel="Download"
+                className="text-bleu-de-france"
+                type="button"
+                light
+                onClick={downloadFile}
+              >
+                <DownloadIcon className="stroke-bleu-de-france" />
+                <div>Download</div>
+              </Button>
+              <Button ariaLabel="Print" className="text-orchid" type="button" light>
+                <PrintIcon className="stroke-orchid" />
+                <div>Print</div>
+              </Button>
+            </div>
           </div>
         </Modal>
       )}
