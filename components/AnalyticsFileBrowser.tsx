b2e8c3560fec6429088ae0ca36566f0dab9a3b98
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-08-11 08:51:32 +0800

    update analytics file browser

--- components/AnalyticsFileBrowser.tsx
+++ components/AnalyticsFileBrowser.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
+import { focusManager, useQuery } from 'react-query'
 import { Files } from '../types/Files.type'
 import { Folder } from '../types/Folder.type'
 import { Card } from './Card'
@@ -13,18 +13,19 @@ import { UploadFileModal, useUploadFileModal } from './modals/UploadFileModal'
 export const AnalyticsFileBrowser = ({ clientId }: { clientId: number }) => {
   const { toggleUploadFileModal } = useUploadFileModal()
   const { toggleShowPhotoVideoFileModal } = useFileDisplayModalStore()
+  const [currentFolderFilesAndId, setCurrentFolderFilesAndId] = useState<{
+    folder: Folder
+    files: Files
+    id: number
+  }>({
+    folder: {},
+    files: {},
+    id: -1,
+  })
+  const [foldersStack, setFoldersStack] = useState<Array<string>>([])
+  const [filesStack, setFilesStack] = useState<Array<string>>([])
 
-  const [foldersStack, setFoldersStack] = useState<
-    Array<{
-      key: string
-      id: number
-      files: Files
-    }>
-  >([])
-  const [yearFolder, setYearFolder] = useState('')
-  const [monthFolder, setMonthFolder] = useState('')
-
-  const { data: folder, isSuccess: filesSuccess } = useQuery(
+  const { data, isSuccess, isRefetching } = useQuery(
     ['clientFiles', clientId],
     async () => {
       const { data } = await axios.get<Folder>(`/v1/clients/${clientId}/analytics-folder`)
@@ -36,154 +37,143 @@ export const AnalyticsFileBrowser = ({ clientId }: { clientId: number }) => {
     }
   )
 
-  const goUpOneFolder = () =>
-    monthFolder
-      ? setMonthFolder('')
-      : yearFolder
-      ? setYearFolder('')
-      : setFoldersStack([...foldersStack].slice(0, -1))
-
   useEffect(() => {
+    setFilesStack([])
     setFoldersStack([])
-    setYearFolder('')
-    setMonthFolder('')
   }, [clientId])
 
   useEffect(() => {
-    if (folder && foldersStack.length > 0) {
-      const newFolderStack: Array<{
-        key: string
-        id: number
-        files: Files
-      }> = []
+    if (data) {
+      setCurrentFolderFilesAndId(
+        foldersStack.reduce(
+          (folderAndFiles, currentFolder) => {
+            if (!Array.isArray(folderAndFiles.folder[currentFolder].files))
+              folderAndFiles.files = folderAndFiles.folder[currentFolder].files
+            else folderAndFiles.files = {}
 
-      let currentFolder: Folder = {}
+            folderAndFiles.id = folderAndFiles.folder[currentFolder]
+              ? folderAndFiles.folder[currentFolder].id
+              : -1
 
-      foldersStack.forEach(({ key }, i) => {
-        if (i === 0) {
-          currentFolder = folder[key].folders.reduce(
-            (folders, folder) => ({ ...folders, ...folder }),
-            {}
-          )
-          newFolderStack.push({
-            key,
-            id: folder[key].id,
-            files: folder[key].files,
-          })
-        } else {
-          currentFolder = currentFolder[key].folders.reduce(
-            (folders, folder) => ({ ...folders, ...folder }),
-            {}
-          )
-          newFolderStack.push({
-            key,
-            id: currentFolder[key].id,
-            files: currentFolder[key].files,
-          })
-        }
-      })
+            if (
+              folderAndFiles.folder[currentFolder].folders &&
+              folderAndFiles.folder[currentFolder].folders.length > 0
+            )
+              folderAndFiles.folder = folderAndFiles.folder[currentFolder].folders.reduce(
+                (folders, folder) => ({ ...folders, ...folder }),
+                {}
+              )
+            else folderAndFiles.folder = {}
 
-      setFoldersStack(newFolderStack)
+            return folderAndFiles
+          },
+          {
+            folder: data,
+            files: {} as Files,
+            id: -1,
+          }
+        )
+      )
     }
-  }, [folder])
+  }, [foldersStack, data])
+
+  useEffect(() => {
+    focusManager.setFocused(false)
+    return () => focusManager.setFocused(undefined)
+  }, [])
 
   return (
     <>
-      <Card className="h-fit flex-1">
-        {clientId !== -1 && !yearFolder && foldersStack.length > 0 && (
+      <Card className={`h-fit flex-1 transition-opacity${isRefetching ? ' opacity-50' : ''}`}>
+        {clientId !== -1 && filesStack.length === 0 && foldersStack.length > 0 && (
           <div className="absolute top-6 right-6 flex space-x-5">
             <button
               className="flex space-x-2"
-              onClick={() => toggleUploadFileModal(foldersStack[foldersStack.length - 1].id)}
+              onClick={() => toggleUploadFileModal(currentFolderFilesAndId.id)}
             >
               <AddFileIcon className="stroke-halloween-orange" />
               <div className=" text-sm font-semibold text-halloween-orange">Upload File</div>
             </button>
           </div>
         )}
-        <div className="flex max-h-155 flex-wrap gap-4 overflow-y-auto pt-10">
-          {clientId !== -1 ? (
-            filesSuccess && (
-              <>
-                {foldersStack.length > 0 && (
+        <div className="mt-10 flex max-h-155 flex-wrap gap-4 overflow-y-auto">
+          {clientId !== -1 && isSuccess && (
+            <>
+              {foldersStack.length > 0 && (
+                <FileButton
+                  disabled={isRefetching}
+                  name="Go back"
+                  Icon={<CaretIcon className="-rotate-90 stroke-halloween-orange" />}
+                  onClick={() => {
+                    if (filesStack.length === 0) setFoldersStack([...foldersStack].slice(0, -1))
+                    else setFilesStack([...filesStack].slice(0, -1))
+                  }}
+                />
+              )}
+              {currentFolderFilesAndId &&
+                filesStack.length === 0 &&
+                Object.entries(currentFolderFilesAndId.folder).map(([k, { id, name }]) => (
                   <FileButton
-                    name="Go back"
-                    Icon={<CaretIcon className="-rotate-90 stroke-halloween-orange" />}
-                    onClick={goUpOneFolder}
+                    disabled={isRefetching}
+                    key={name}
+                    name={name}
+                    onClick={() => setFoldersStack([...foldersStack, k])}
+                    folderId={id}
+                    folderName={name}
                   />
-                )}
-                {!yearFolder &&
-                  Object.entries(
-                    foldersStack.reduce((currentFolder, { key }) => {
-                      const folders = currentFolder[key].folders
-
-                      if (folders && !Array.isArray(folders[0])) {
-                        return folders.reduce((folders, folder) => ({ ...folders, ...folder }), {})
-                      }
-
-                      return {}
-                    }, folder)
-                  ).map(([k, { id, name, files }]) => {
-                    const enterFolder = () =>
-                      setFoldersStack([
-                        ...foldersStack,
-                        {
-                          key: k,
-                          id: id,
-                          files,
-                        },
-                      ])
-
-                    return (
-                      <FileButton
-                        key={name}
-                        name={name}
-                        onClick={enterFolder}
-                        folderId={id}
-                        folderName={name}
-                      />
-                    )
-                  })}
-                {!yearFolder &&
-                  foldersStack.length > 0 &&
-                  Object.keys(foldersStack[foldersStack.length - 1].files).map((year) => {
-                    const enterYearFolder = () => setYearFolder(year)
-
-                    return <FileButton key={year} name={year} onClick={enterYearFolder} />
-                  })}
-                {yearFolder &&
-                  !monthFolder &&
-                  Object.keys(foldersStack[foldersStack.length - 1].files[yearFolder]).map(
-                    (month) => {
-                      const enterMonthFolder = () => setMonthFolder(month)
-
-                      return (
+                ))}
+              {currentFolderFilesAndId && filesStack.length === 0
+                ? Object.keys(currentFolderFilesAndId.files).map((year) => (
+                    <FileButton
+                      disabled={isRefetching}
+                      key={year}
+                      name={year}
+                      onClick={() => setFilesStack([...filesStack, year])}
+                    />
+                  ))
+                : filesStack.length === 1
+                ? Object.keys(currentFolderFilesAndId.files[filesStack[0]]).map((month) => (
+                    <FileButton
+                      disabled={isRefetching}
+                      key={month}
+                      name={`${month.charAt(0).toUpperCase()}${month.slice(1)}`}
+                      onClick={() => setFilesStack([...filesStack, month])}
+                    />
+                  ))
+                : filesStack.length === 2 &&
+                  Object.values(currentFolderFilesAndId.files[filesStack[0]][filesStack[1]]).map(
+                    ({
+                      fileName,
+                      originalFilename,
+                      thumbnailUrl,
+                      clientTicketFile,
+                      url,
+                      fileType,
+                    }) =>
+                      clientTicketFile ? (
                         <FileButton
-                          key={month}
-                          name={`${month.charAt(0).toUpperCase()}${month.slice(1)}`}
-                          onClick={enterMonthFolder}
+                          disabled={isRefetching}
+                          key={fileName}
+                          name={originalFilename}
+                          thumbnailUrl={thumbnailUrl}
+                          href={`/ticket/file/${clientTicketFile.id}`}
+                          fileStatus={clientTicketFile.status}
+                          file
+                        />
+                      ) : (
+                        <FileButton
+                          disabled={isRefetching}
+                          key={fileName}
+                          name={originalFilename}
+                          thumbnailUrl={thumbnailUrl}
+                          onClick={() =>
+                            toggleShowPhotoVideoFileModal(url, fileType, originalFilename)
+                          }
+                          file
                         />
                       )
-                    }
                   )}
-                {monthFolder &&
-                  foldersStack[foldersStack.length - 1].files[yearFolder][monthFolder].map(
-                    ({ fileName, originalFilename, thumbnailUrl, url, fileType }) => (
-                      <FileButton
-                        key={fileName}
-                        name={originalFilename}
-                        thumbnailUrl={thumbnailUrl}
-                        onClick={() =>
-                          toggleShowPhotoVideoFileModal(url, fileType, originalFilename)
-                        }
-                        file
-                      />
-                    )
-                  )}
-              </>
-            )
-          ) : (
-            <div className="flex-1 text-center text-xs text-metallic-silver">Select a Client.</div>
+            </>
           )}
         </div>
       </Card>
