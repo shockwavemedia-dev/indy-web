cdaccb0de0cea9b79ae6e7b9c8f4e0a29f43e340
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-08-23 09:28:22 +0800

    printer data table and store

---
+++ constants/tables/AdminPrintersTableColumns.tsx
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+import { Column } from 'react-table'
+import { EditIcon } from '../../components/icons/EditIcon'
+import { TrashIcon } from '../../components/icons/TrashIcon'
+import { useAdminPrinterStore } from '../../store/AdminPrinterStore'
+import { Printer } from '../../types/Printer.type'
+
+export const AdminPrintersTableColumns: Array<Column<Printer>> = [
+  {
+    Header: 'Company',
+    accessor: 'companyName',
+    Cell: ({ value }) => <div className=" text-sm font-semibold capitalize text-onyx">{value}</div>,
+  },
+  {
+    Header: 'Logo',
+    accessor: 'companyThumbnailLogoUrl',
+    Cell: ({ value }) => (
+      <>
+        {value && (
+          <div className="rounded-md">
+            <Image src={value} alt={value} height={40} width={40} />
+          </div>
+        )}
+      </>
+    ),
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+    Cell: ({ value }) => <div className=" text-sm font-semibold text-onyx">{value}</div>,
+  },
+  {
+    Header: 'Contact Name',
+    accessor: 'contactName',
+    Cell: ({ value }) => <div className=" text-sm font-semibold capitalize text-onyx">{value}</div>,
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+    Cell: ({ value }) => <div className=" text-sm font-semibold capitalize text-onyx">{value}</div>,
+  },
+  {
+    Header: 'Actions',
+    accessor: 'id',
+    disableSortBy: true,
+    Cell: ({ row: { original: printer } }) => {
+      const { setActiveAdminPrinter, toggleEditAdminPrinterModal, toggleDeleteAdminPrinterModal } =
+        useAdminPrinterStore()
+
+      const editAdminPrinter = () => {
+        setActiveAdminPrinter(printer)
+        toggleEditAdminPrinterModal()
+      }
+
+      const deleteAdminPrinter = () => {
+        setActiveAdminPrinter(printer)
+        toggleDeleteAdminPrinterModal()
+      }
+
+      return (
+        <div className="flex space-x-2">
+          <button onClick={editAdminPrinter} className="group">
+            <EditIcon className="stroke-waterloo group-hover:stroke-halloween-orange" />
+          </button>
+          <button onClick={deleteAdminPrinter} className="group">
+            <TrashIcon className="stroke-waterloo group-hover:stroke-halloween-orange" />
+          </button>
+        </div>
+      )
+    },
+  },
+]
--- pages/printers.tsx
+++ pages/printers.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import { ReactElement, useEffect, useState } from 'react'
 import { Card } from '../components/Card'
+import { DataTable } from '../components/DataTable'
 import { FancyButton } from '../components/FancyButton'
 import { MonitorIcon } from '../components/icons/MonitorIcon'
 import { CreatePrinterModal } from '../components/modals/CreatePrinterModal'
+import { AdminPrintersTableColumns } from '../constants/tables/AdminPrintersTableColumns'
 import PanelLayout, { usePanelLayoutStore } from '../layouts/PanelLayout'
 import { NextPageWithLayout } from '../types/pages/NextPageWithLayout.type'
 
@@ -30,20 +32,23 @@ const PrintersPage: NextPageWithLayout = () => {
 
   return (
     <>
-      <CreatePrinterModal
-        isVisible={isCreatePrinterModalVisible}
-        onClose={toggleCreatePrinterModal}
-      />
       <Head>
         <title>Indy - Printers</title>
       </Head>
       <div className="mx-auto w-full max-w-8xl space-y-6">
-        <div className="flex gap-6 transition-all lg:flex-col">
-          <Card className="grid h-155 flex-1 place-items-center transition-all lg:flex-none">
-            <div className="">Nothing to see here. 🦗</div>
-          </Card>
-        </div>
+        <Card title="Printers List" className="flex max-h-155 flex-col">
+          <DataTable
+            dataEndpoint="/v1/printers"
+            columns={AdminPrintersTableColumns}
+            tableQueryKey={['printers']}
+            ofString="Printers"
+          />
+        </Card>
       </div>
+      <CreatePrinterModal
+        isVisible={isCreatePrinterModalVisible}
+        onClose={toggleCreatePrinterModal}
+      />
     </>
   )
 }
