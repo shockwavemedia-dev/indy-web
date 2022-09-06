48c33278070d1929d2f4f74fbd1ac000c7086398
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-09-06 11:33:22 +0800

    update client

--- types/forms/EditClientForm.type.ts
+++ types/forms/EditClientForm.type.ts
@@ -1,3 +1,13 @@
-import { NewClientForm } from './NewClientForm.type'
-
-export type EditClientForm = NewClientForm
+export type EditClientForm = {
+  name: string
+  clientCode: string
+  logo: File | null
+  address: string
+  phone: string
+  timezone: string
+  overview: string
+  clientSince: Date | null
+  rating: number | null
+  designatedDesignerId?: number
+  _method: string
+}
