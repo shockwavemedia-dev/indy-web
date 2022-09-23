38be040f414a65ceacee10e1be6d568a6fa4547c
Mark <mark@shockwavemedia.com.au>
2022-09-23 08:19:50 +0800

    - update user form type

--- types/forms/NewClientUserForm.type.ts
+++ types/forms/NewClientUserForm.type.ts
@@ -1,13 +1,10 @@
 export type NewClientUserForm = {
   email: string
   password: string
-  birthDate: Date | null
   passwordConfirmation: string
   contactNumber: string
   firstName: string
-  lastName: string
-  middleName?: string
-  gender: 'female' | 'male' | null
+  lastName?: string
   role: 'group manager' | 'marketing manager' | 'marketing' | null
   clientId: number
 }
