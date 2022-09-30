3c6495228fce96c2f61dbbc8519c5a0fe6bf96fe
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-09-30 13:51:05 +0800

    update notif modal and remove calendar

--- components/SocialMediaNotificationModal.tsx
+++ components/SocialMediaNotificationModal.tsx
@@ -28,8 +28,8 @@ export const SocialMediaNotificationModal = () => {
           <div
             className={`absolute top-15 -right-35 z-50 flex -translate-x-1/2 flex-col items-center rounded-xl bg-white p-5`}
           >
-            <button className="absolute top-2 right-2" onClick={() => toggleModal()}>
-              <CloseModalIcon width="20" height="20" className="stroke-lavender-gray" />
+            <button className="absolute top-2 right-3" onClick={() => toggleModal()}>
+              <CloseModalIcon width="15" height="15" className="stroke-lavender-gray" />
             </button>
             <Notifications className="w-86 lg:w-1/2" />
           </div>
