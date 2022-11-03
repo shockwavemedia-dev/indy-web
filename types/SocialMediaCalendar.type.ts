62a7213591c635a1640904de8a7158dabd21cfed
Kyle Oliva <kyle.oliva@dailypress.com.au>
2022-11-04 07:39:23 +0800

    New social media calendar

--- types/SocialMediaCalendar.type.ts
+++ types/SocialMediaCalendar.type.ts
@@ -1,16 +1,3 @@
-export type SocialMediaCalendar = Record<
-  'string',
-  Array<{
-    id: number
-    clientId: number
-    post: string
-    copy?: string | null
-    status: string
-    channels?: Array<string> | null
-    notes?: string
-    postDate: Date
-    createdBy: number
-    updatedBy: number
-    campaignType?: string | null
-  }>
->
+import { SocialMedia } from './SocialMedia.type'
+
+export type SocialMediaCalendar = Record<string, Array<SocialMedia>>
