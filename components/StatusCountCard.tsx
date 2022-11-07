66545ead3595bc4b7d88d8e03ef6ece6cec40aee
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-11-07 14:00:23 +0800

    retainer change from static to dynamic

--- components/StatusCountCard.tsx
+++ components/StatusCountCard.tsx
@@ -2,13 +2,17 @@ export const StatusCountCard = ({
   value,
   description,
   className,
+  classNameValue,
 }: {
-  value: number
+  value: number | string
   description: string
   className?: string
+  classNameValue?: string
 }) => (
   <div className={`grid place-content-center rounded-xl py-2 ${className}`}>
-    <div className="mb-1 text-center text-xl font-semibold text-white">{value}</div>
+    <div className={`mb-1 text-center text-xl font-semibold text-white ${classNameValue}`}>
+      {typeof value === 'number' ? Math.abs(value) : value}
+    </div>
     <div className="px-1 text-center text-xs font-medium text-white">{description}</div>
   </div>
 )
