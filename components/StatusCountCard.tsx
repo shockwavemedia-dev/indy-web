fc23c2599d0f86b5df3b6738d281e6925cc99365
Mark <mark@shockwavemedia.com.au>
2022-11-11 07:53:57 +0800

    - optional total value

--- components/StatusCountCard.tsx
+++ components/StatusCountCard.tsx
@@ -9,7 +9,7 @@ export const StatusCountCard = ({
   description: string
   className?: string
   classNameValue?: string
-  totalAvailable: number | string
+  totalAvailable?: number | string
 }) => (
   <div className={`grid place-content-center rounded-xl py-2 ${className}`}>
     <div className={`mb-1 text-center text-xl font-semibold text-white ${classNameValue}`}>
