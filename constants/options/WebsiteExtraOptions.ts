a3ef696f7211562b33bb70ba2802bd25c414afd1
Mark <mark@shockwavemedia.com.au>
2022-11-23 09:23:30 +0800

    - add website extra options

--- constants/options/WebsiteExtraOptions.ts
+++ constants/options/WebsiteExtraOptions.ts
@@ -5,4 +5,5 @@ export const WebsiteExtraOptions: Options<SelectOption<string>> = [
   { label: 'Homepage Header', value: 'Homepage Header' },
   { label: "What's On", value: "What's On" },
   { label: 'Bistro', value: 'Bistro' },
+  { label: 'Custom', value: 'Custom' },
 ]
