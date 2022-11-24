a42f76aa632afaed12d1e8018de22836bd17e65c
Mark <mark@shockwavemedia.com.au>
2022-11-24 09:21:52 +0800

    - default timezone value

--- components/modals/NewClientModal.tsx
+++ components/modals/NewClientModal.tsx
@@ -107,6 +107,17 @@ export const NewClientModal = ({
                       Icon={ClockIcon}
                       placeholder="Enter Timezone"
                       options={TimezoneOptions}
+                      defaultValue={(() => {
+                        const timezone = TimezoneOptions.find(
+                          ({ value }) => value === '(UTC+10:00) Canberra, Melbourne, Sydney'
+                        )
+
+                        if (timezone) {
+                          return timezone
+                        }
+
+                        return undefined
+                      })()}
                     />
                     <DateInput name="clientSince" placeholder="Enter Client Since" />
                   </div>
